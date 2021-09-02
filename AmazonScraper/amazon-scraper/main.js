7b001deb85282dc261a4e549b52b024d896ffd35
Kristýna Lhoťanová <kristyna.posta@seznam.cz>
2021-09-02 21:32:27 +0200

    Add scraping skeleton for detail and offer pages

--- AmazonScraper/amazon-scraper/main.js
+++ AmazonScraper/amazon-scraper/main.js
@@ -1,7 +1,8 @@
 const Apify = require('apify');
-const { handleStart, handleList: handleOffers, handleDetail } = require('./src/routes');
+const { handleStart, handleOffers, handleDetail } = require('./src/routes');
 
 const { utils: { log } } = Apify;
+const result = {};
 
 Apify.main(async () => {
     const input = await Apify.getInput();
@@ -9,25 +10,25 @@ Apify.main(async () => {
 
     const requestList = await Apify.openRequestList('start-url', [startUrl]);
     const requestQueue = await Apify.openRequestQueue();
-    // const proxyConfiguration = await Apify.createProxyConfiguration();
 
     const crawler = new Apify.PuppeteerCrawler({
         maxRequestsPerCrawl: 50,
         requestList,
         requestQueue,
-        // proxyConfiguration,
         launchContext: {
             useChrome: true,
             stealth: true,
         },
         handlePageFunction: async (context) => {
             const { url, userData: { label } } = context.request;
+
             log.info('Page opened.', { label, url });
+
             switch (label) {
                 case 'OFFERS':
-                    return handleOffers(context);
+                    return handleOffers(context, result);
                 case 'DETAIL':
-                    return handleDetail(context);
+                    return handleDetail(context, result);
                 default:
                     return handleStart(context, requestQueue);
             }
--- AmazonScraper/amazon-scraper/src/routes.js
+++ AmazonScraper/amazon-scraper/src/routes.js
@@ -12,7 +12,7 @@ exports.handleStart = async ({ page }, requestQueue) => {
 
         const elements = document.querySelectorAll('div[data-asin]');
         elements.forEach((element) => {
-            const ASIN = element.getAttributeNode('data-asin').value;
+            const ASIN = element.getAttribute('data-asin');
             if (ASIN !== '') ASINs.push(ASIN);
         });
 
@@ -20,15 +20,50 @@ exports.handleStart = async ({ page }, requestQueue) => {
     });
 
     log.info(`Product ASINs: ${productASINs}`);
-    await enqueueDetailPages(productASINs, requestQueue);
+    await enqueuePagesToScrape(productASINs, requestQueue);
 };
 
-exports.handleOffers = async ({ request, page }) => {
+exports.handleOffers = async ({ request, page }, result) => {
+    const sellerName = await getElementInnerText(page, '#sellerProfileTriggerId');
+    const price = await getElementInnerText(page, '#price_inside_buybox');
+    const shippingPrice = await getShippingPrice(page);
 
+    const { ASIN } = request.userData;
+
+    const detailResult = { ...result[ASIN] };
+    const offerResult = { sellerName, price, shippingPrice };
+
+    const joinedResult = {
+        ...detailResult,
+        ...offerResult,
+    };
+
+    log.info('Joined result is: ');
+    log.info(`Title: ${joinedResult.title}, Description: ${joinedResult.description}, Url: ${joinedResult.url}`);
+    log.info(`Seller name: ${joinedResult.sellerName}, Price: ${joinedResult.price}, Shipping: ${joinedResult.shippingPrice}`);
+
+    result[ASIN].offers.push(joinedResult);
+    await Apify.pushData(joinedResult);
 };
 
-exports.handleDetail = async ({ request, page }) => {
+exports.handleDetail = async ({ request, page }, result) => {
+    log.info('Crawling detail page');
 
+    const title = await getElementInnerText(page, '#productTitle');
+    const description = await getElementInnerText(page, '#productDescription');
+
+    const input = await Apify.getInput();
+    const { keyword } = input;
+
+    const { url } = request;
+    const { ASIN } = request.userData;
+
+    result[ASIN] = { title, description, url, keyword };
+
+    log.info(`New detail saved. Title: ${result[ASIN].title},
+    Description: ${result[ASIN].description},
+    Url: ${result[ASIN].url},
+    Keyword: ${result[ASIN].keyword}`);
 };
 
 /**
@@ -36,13 +71,38 @@ exports.handleDetail = async ({ request, page }) => {
  * @param {Array} productASINs
  * @param {Apify.RequestQueue} requestQueue
  */
-async function enqueueDetailPages(productASINs, requestQueue) {
+async function enqueuePagesToScrape(productASINs, requestQueue) {
     productASINs.forEach(async (ASIN) => {
-        const request = new Apify.Request({
+        const detailRequest = new Apify.Request({
             url: `https://www.amazon.com/dp/${ASIN}`,
-            userData: { label: 'DETAIL' },
+            userData: { label: 'DETAIL', ASIN },
         });
 
-        await requestQueue.addRequest(request);
+        const offersRequest = new Apify.Request({
+            url: `https://www.amazon.com/gp/offer-listing/${ASIN}`,
+            userData: { label: 'OFFERS', ASIN },
+        });
+
+        await requestQueue.addRequest(detailRequest);
+        await requestQueue.addRequest(offersRequest);
     });
+}
+
+async function getElementInnerText(page, selector) {
+    return page.$eval(selector, (el) => el.innerText);
+}
+
+async function getShippingPrice(page) {
+    const shippingInfo = await getElementInnerText(page, '#ourprice_shippingmessage>span.a-size-base');
+    log.info(`Shipping info: ${shippingInfo}`);
+
+    if (shippingInfo === null || shippingInfo.length === 0) return null;
+
+    const fragments = shippingInfo.split(' ');
+    const shippingKeywordIndex = fragments.indexOf('Shipping');
+    const shippingPriceIndex = shippingKeywordIndex - 1;
+
+    if (shippingPriceIndex < fragments.length) return null;
+
+    return fragments[shippingPriceIndex];
 }
