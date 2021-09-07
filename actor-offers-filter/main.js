b719b7d7357530d151a2a725d7d539cdd20d20a4
Kristýna Lhoťanová <kristyna.posta@seznam.cz>
2021-09-07 14:27:03 +0200

    Edit offers filter actor for testing purposes

--- actor-offers-filter/main.js
+++ actor-offers-filter/main.js
@@ -1,8 +1,3 @@
-// This is the main Node.js source code file of your actor.
-// It is referenced from the "scripts" section of the package.json file,
-// so that it can be started by running "npm start".
-
-// Import Apify SDK. For more information, see https://sdk.apify.com/
 const Apify = require('apify');
 
 Apify.main(async () => {
@@ -13,6 +8,6 @@ Apify.main(async () => {
     // Do something useful here...
     const cheapestOffers = [];
 
-    
-    await Apify.pushData(cheapestOffers);
+
+    await Apify.pushData(input);
 });
